c31ed58c92e206e334b0d21caaefd041dde64310
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-07-09 19:44:20 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash.each do |time_of_year,data|
     puts "#{time_of_year.capitalize}:"
       data.each do | key, value|
-        puts "  #{key.capitalize}: #{value.join}"
+        puts "  #{key.capitalize}: #{value.join(", ")}"
       end
   end
   
